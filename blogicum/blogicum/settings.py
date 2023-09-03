3f6afa0cd5b23c04a4505a621b44c1c6efdbe4ac
Farrukh <frustamov@openwaygroup.com>
2023-09-03 17:21:53 +0200

    fixed pytest issues

--- blogicum/blogicum/settings.py
+++ blogicum/blogicum/settings.py
@@ -27,7 +27,6 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
-
 # Application definition
 
 INSTALLED_APPS = [
@@ -107,7 +106,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/3.2/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'ru-RU'
 
 TIME_ZONE = 'UTC'
 
